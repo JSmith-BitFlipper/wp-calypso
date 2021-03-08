6ba053e96d83cc0a38b28858c7e63e6d9352a614
Sergio Cinos <sergio.cinos@gmail.com>
2021-03-08 08:25:57 +0100

    Add `wpcom` and `calypso` browserlist envs

--- packages/calypso-build/webpack/minify.js
+++ packages/calypso-build/webpack/minify.js
@@ -112,7 +112,9 @@ module.exports = ( options ) => {
 	terserOptions = {
 		ecma: chooseTerserEcmaVersion( supportedBrowsers ),
 		ie8: false,
-		safari10: false,
+		safari10: supportedBrowsers.some(
+			( browser ) => browser.includes( 'safari 10' ) || browser.includes( 'ios_saf 10' )
+		),
 		...terserOptions,
 	};
 
